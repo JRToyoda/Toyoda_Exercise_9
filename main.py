e51be9f355698c67da9aa58afa0e5d909a40bda7
JRToyoda <therubikators@gmail.com>
2023-01-08 18:51:37 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Letter:
+    def __init__(self, letterFrom, letterTo):
+        self.letterFrom = letterFrom
+        self.letterTo = letterTo
+        self.body = []
+
+    def addLine(self, line):
+        self.body.append(line)
+
+    def getText(self):
+        text = "Dear " + self.letterTo + ":\n"
+        text += "\n"
+
+        for line in self.body:
+            text = text + line + "\n"
+            text += "\n"
+
+        text += "Sincerely,\n"
+        text += self.letterFrom
+
+        return text
---
+++ package.py
@@ -0,0 +1,6 @@
+from main import Letter
+
+my_message = Letter("Mary", "John")
+my_message.addLine("I am sorry we must part.\nI wish you all the best.")
+
+print(my_message.getText())
